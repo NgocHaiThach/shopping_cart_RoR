c46342bd5fcb637f8418a707f42080575aa7eaf7
NgocHaiThach <ngchai2410@gmail.com>
2022-09-29 14:17:54 +0700

    done login and remember user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,43 @@
 class User < ApplicationRecord
+
+    attr_accessor :remember_token 
+    
+    #validates
     before_save { email.downcase! }
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-    # VALID_PHONE_REGEX = (84|0[3|5|7|8|9])+([0-9]{8})\b
-    # validates :phone, 
-    # format: {with: VALID_PHONE_REGEX}
-    # validates :address, length: {maximum: 255}
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+    #class method
+    def User.digest (string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost:cost)
+    end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # def authenticated?(attribute, token)
+    #     digest = send("#{attribute}_digest")
+    #     return false if remember_digest.nil?
+    #     BCrypt::Password.new(digest).is_password?(token)
+    # end
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+    end
+
+    def forget 
+        update_attribute(:remember_digest, nil)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_29_023824) do
+ActiveRecord::Schema.define(version: 2022_09_29_064102) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_09_29_023824) do
     t.boolean "shop_owner"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
